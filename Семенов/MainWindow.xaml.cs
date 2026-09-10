b45f5549e8317bda020e4644ade2e9344723c4cc
User <User@WS-10>
2026-09-10 13:00:38 +0500

    Добавьте файлы проекта.

---
+++ Семенов/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace Семенов
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnShowClick(object sender, RoutedEventArgs e)
+        {
+            string input = InputBox.Text;
+            ResultLabel.Text = string.IsNullOrEmpty(input)
+                ? "Вы ничего не ввели."
+                : "Вы ввели: " + input;
+        }
+    }
+}
